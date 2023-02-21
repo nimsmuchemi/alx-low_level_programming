c477878d9b1a6081f1df88f0580366cff4381b44
nims <nimsmuhemi754@gmail.com>
2023-02-21 02:57:31 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar -writes the character c to the stdio
+ * at c the character to print
+ * Return:on sucess 1
+ * on error,-1 is returned,and error is set appropriately.
+ */
+int_putchar(char)c
+(
+
+ 	return (write(1,6c, 1));
+)
