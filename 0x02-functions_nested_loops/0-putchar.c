21556cd9ae8edf2a36e3eccc16420e7bea645211
nims <nimsmuhemi754@gmail.com>
2023-02-21 04:50:22 -0800

    task 0 and 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
- * _putchar -writes the character c to the stdio
- * at c the character to print
- * Return:on sucess 1
- * on error,-1 is returned,and error is set appropriately.
- */
-int_putchar(char)c
-(
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
 
- 	return (write(1,6c, 1));
-)
+int main(void)
+{
+    char str[] = "_putchar";
+    int chr;
+
+    for (chr = 0; chr < 8; ++chr)
+        _putchar(str[chr]);
+    _putchar('\n');
+
+    return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,20 @@
-int _punchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
+#include<stdio.h>
+
+int _putchar(char c);
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int print_sign(int n);
-
-inti _abs(int);
-
+int _abs(int);
 int print_last_digit(int);
-
-void jack_baurer(void);
-
+void jack_bauer(void);
 void times_table(void);
-
 int add(int, int);
-
 void print_to_98(int n);
-
 void print_times_table(int n);
+
+#endif /* MAIN_H */
