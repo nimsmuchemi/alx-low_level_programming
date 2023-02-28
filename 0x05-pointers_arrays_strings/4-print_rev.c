a093f012ee5904014ebd8a9baa366fcb8766aea9
nims <nimsmuhemi754@gmail.com>
2023-02-28 15:11:27 -0800

    reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+
+ *print_int -  function that prints a string, in reverse, followed by a new line 
+
+ *@a: interger to print
+ *@b interger to print
+*/
+ void print_rev(char *s);
+(
+	
+
