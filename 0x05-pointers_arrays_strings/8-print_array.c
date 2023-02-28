cf828427aac11a294852b755bb6a230e89041e18
nims <nimsmuhemi754@gmail.com>
2023-02-28 15:31:39 -0800

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+
+ *print_int -  that prints n elements of an array of integers, followed by a new line.
+
+ *@a: interger to print
+ *@b interger to print
+*/
+ void print_array(int *a, int n);
+ *where n is the number of elements of the array to be printed Numbers must be separated by comma, followed by a space The numbers should be displayed in the same order as they are stored in the array You are allowed to use printf
+
