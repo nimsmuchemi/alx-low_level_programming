79c45df21d65f47e377d6a5680ebe65dbeee8ba8
nims <nimsmuhemi754@gmail.com>
2023-02-28 15:23:02 -0800

    arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+
+ *print_int - print every other character of a string, starting with the first character, followed by a new line.
+
+ *@a: interger to print
+ *@b interger to print
+*/
+ void puts2(char *str);
